1ccbb050bb6a57f9b9b544228677cff8bf413764
baoispro <nguyenlegiabao810@gmail.com>
2024-11-29 16:22:54 +0700

    add backend

---
+++ backend/Product.Core/Interface/IProductRepository.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Product.Core.Entities;
+using Product.Core.Sharing;
+using Product.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Core.Interface
+{
+    public interface IProductRepository : IGenericRepository<Products>
+    {
+
+       // Task<IEnumerable<ProductDto>> GetAllAsync(ProductParams productParams);
+        Task<ReturnProductDto> GetAllAsync(ProductParams productParams);
+        Task<bool> AddAsync(CreateProductDto dto);
+        Task<bool> UpdateAsync(int id, UpdateProductDto dto);
+        
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
